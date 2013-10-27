e66a1e82360ad8f6c36c394b058831ad919643c9
vaibhav <vaibhav@chargebee.com>
2013-10-27 20:37:57 +0530

    minor

--- charge.php
+++ charge.php
@@ -1,40 +1,63 @@
 <?php
-  require_once('./header.php');
 
-  if ($_POST) {
+require_once('./header.php');
+require('./config.php'); // will produce a fatal error (E_COMPILE_ERROR) and stop the script
+#include('./config.php'); // include will only produce a warning (E_WARNING) and the script will continue
+
+if ($_POST) {
     $error = NULL;
     try {
-      if (!isset($_POST['stripeToken']))
-        throw new Exception("The Stripe Token was not generated correctly");
-      $charge = Stripe_Charge::create(array(
-        'card'     => $_POST['stripeToken'],
-        'amount'   => 53500,
-        'currency' => 'usd'
-      ));
-    }
-    catch (Exception $e) {
-      $error = $e->getMessage();
+        if (!isset($_POST['stripeToken'])) {
+            throw new Exception("The Stripe Token was not generated correctly");
+        }
+        $stripeToken = $_POST['stripeToken'];
+        $plan = $_POST['plan_id'];
+        $email = $_POST['email'];
+        $firstName = $_POST['first_name'];
+        $lastName = $_POST['last_name'];
+        $phone = $_POST['phone'];
+
+        $result = createSub($plan, $email, $firstName, $lastName, $phone, $stripeToken);
+    } catch (Exception $e) {
+        $error = $e->getMessage();
     }
 
     if ($error == NULL) {
-      $wildeQuotes = array(
-        "A little sincerity is a dangerous thing, and a great deal of it is absolutely fatal.",
-        "Always forgive your enemies; nothing annoys them so much.",
-        "America is the only country that went from barbarism to decadence without civilization in between.",
-        "I think that God in creating Man somewhat overestimated his ability.",
-        "I am not young enough to know everything.",
-        "Fashion is a form of ugliness so intolerable that we have to alter it every six months.",
-        "Most modern calendars mar the sweet simplicity of our lives by reminding us that each day that passes is the anniversary of some perfectly uninteresting event.",
-        "Scandal is gossip made tedious by morality."
+        $wildeQuotes = array(
+            "A little sincerity is a dangerous thing, and a great deal of it is absolutely fatal.",
+            "Always forgive your enemies; nothing annoys them so much.",
+            "America is the only country that went from barbarism to decadence without civilization in between.",
+            "I think that God in creating Man somewhat overestimated his ability.",
+            "I am not young enough to know everything.",
+            "Fashion is a form of ugliness so intolerable that we have to alter it every six months.",
+            "Most modern calendars mar the sweet simplicity of our lives by reminding us that each day that passes is the anniversary of some perfectly uninteresting event.",
+            "Scandal is gossip made tedious by morality."
         );
 
-      echo "<h1>Here's your quote!</h1>";
-      echo "<h2>".$wildeQuotes[array_rand($wildeQuotes)]."</h2>";
+        echo "<h1>Here's your quote!</h1>";
+        echo "<h2>" . $wildeQuotes[array_rand($wildeQuotes)] . "</h2>";
+    } else {
+        require_once('./payment_form.php');
+        echo "<script type=\"text/javascript\">$(\".payment-errors\").html(\"$error\");</script>";
     }
-    else {
-      require_once('./payment_form.php');
-      echo "<script type=\"text/javascript\">$(\".payment-errors\").html(\"$error\");</script>";
-    }
-  }
-  require_once('./footer.php');
+}
+require_once('./footer.php');
+?>
+
+<?php
+
+function createSub($plan, $email, $firstName, $lastName, $phone, $stripeToken) {
+    $result = ChargeBee_Subscription::create(array(
+                "planId" => $plan,
+                "customer" => array(
+                    "email" => $email,
+                    "firstName" => $firstName,
+                    "lastName" => $lastName,
+                    "phone" => $phone
+                ),
+                "card" => array(
+                    "tmp_token" => $stripeToken
+    )));
+    return $result;
+}
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<?php phpinfo() ?>
+
 <?php
-  require_once('./header.php');
-  require_once('./payment_form.php');
-  require_once('./footer.php');
+$inipath = php_ini_loaded_file();
+if ($inipath) {
+    echo 'Loaded php.ini: ' . $inipath;
+} else {
+    echo 'A php.ini file is not loaded';
+}
+?>
+<?php require_once('./header.php');?>
+<?php require_once('./payment_form.php');?>
+Hello
+<?php
+include './footer. php';
 ?>
