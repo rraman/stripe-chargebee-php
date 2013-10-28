3e9603cdcfcb8d3e4b84935a7b67cd34d15d9535
cbdtn <dtn@chargebee.com>
2013-10-28 19:00:53 +0530

    ui changes

--- order_summary.php
+++ order_summary.php
@@ -82,6 +82,7 @@
           <input class="btn btn-info" type="submit" value="Apply Coupon">
           </span>
         </div>
+        <span class="process" style="display:none;">Processing&hellip;</span>
         <small for="coupon" class="text-danger"></small>
         </form>
       </div>
