dd88ac01f6e87add62895cedb07a96da04b529f8
vaibhav <vaibhav@chargebee.com>
2013-10-28 10:00:28 +0530

    small

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php phpinfo() ?>
-
-<?php
-$inipath = php_ini_loaded_file();
-if ($inipath) {
-    echo 'Loaded php.ini: ' . $inipath;
-} else {
-    echo 'A php.ini file is not loaded';
-}
-?>
 <?php require_once('./header.php');?>
 <?php require_once('./payment_form.php');?>
 Hello
